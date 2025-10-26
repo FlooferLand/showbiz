5fab98043dee904ad8ae5bf9a49f771a824c8034
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2025-10-26 11:37:22 +0200

    Add Freddy Rizzbear, Chica The Kitchen, Guitar The uhh Bunny

--- src/main/kotlin/com/flooferland/showbiz/addons/data/AddonBotEntry.kt
+++ src/main/kotlin/com/flooferland/showbiz/addons/data/AddonBotEntry.kt
@@ -1,10 +1,22 @@
 package com.flooferland.showbiz.addons.data
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class AddonBotEntry(
     val name: String,
     val authors: List<String>,
-    val formats: List<String>
+    val accepts: AcceptedFormats,
+
+    @SerialName("allow_missing")
+    val allowMissing: Boolean = false
+)
+
+// TODO: Enforce accepted formats so people can't load fshw files for RAE bots
+@Serializable
+data class AcceptedFormats(
+    val rockafire: Boolean = false,
+    val fnaf1: Boolean = false,
+    val other: Boolean = false
 )
