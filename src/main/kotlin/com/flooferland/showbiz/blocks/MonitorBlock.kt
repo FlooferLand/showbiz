19e9064fe6b57c6724fce6f1534ee9ea55a27ac1
FlooferLand <76737186+FlooferLand@users.noreply.github.com>
2026-09-11 04:21:03 +0300

    Add a link to FFmpeg when right-clicking the TV

--- src/main/kotlin/com/flooferland/showbiz/blocks/MonitorBlock.kt
+++ src/main/kotlin/com/flooferland/showbiz/blocks/MonitorBlock.kt
@@ -1,12 +1,20 @@
 package com.flooferland.showbiz.blocks
 
 import net.minecraft.core.*
+import net.minecraft.network.chat.ClickEvent
+import net.minecraft.network.chat.Component
+import net.minecraft.world.InteractionResult
+import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.context.*
+import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.*
 import net.minecraft.world.level.block.state.*
 import net.minecraft.world.level.block.state.properties.*
+import net.minecraft.world.phys.BlockHitResult
 import com.flooferland.showbiz.blocks.base.FacingEntityBlock
 import com.flooferland.showbiz.registry.ModBlocks
+import com.flooferland.showbiz.types.FFmpeg
+import com.flooferland.showbiz.utils.Extensions.click
 
 class MonitorBlock(props: Properties) : FacingEntityBlock(props) {
     override val codec = simpleCodec(::MonitorBlock)!!
@@ -25,6 +33,15 @@ class MonitorBlock(props: Properties) : FacingEntityBlock(props) {
         val above = context.level.getBlockState(context.clickedPos.above())
         return super.getStateForPlacement(context)
             .setValue(HANGED, !above.isAir)
+    }
+
+    override fun useWithoutItem(state: BlockState, level: Level, pos: BlockPos, player: Player, hitResult: BlockHitResult): InteractionResult? {
+        if (!FFmpeg.serverAvailable) {
+            val comp = Component.literal("You need to install FFmpeg on your server to use the TV")
+                .click(ClickEvent.Action.OPEN_URL, "https://github.com/FlooferLand/showbiz/wiki/Blocks:Monitor");
+            player.displayClientMessage(comp, false)
+        }
+        return super.useWithoutItem(state, level, pos, player, hitResult)
     }
 
     // Generator is bugged
